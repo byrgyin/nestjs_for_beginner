97bef77cfd724d83311b7a860c000d98bea0b5a5
byrgyin <byrgyin@gmail.com>
2026-06-01 17:13:40 +0300

    start_interseptor

---
+++ src/conception/guard.ts
@@ -0,0 +1,12 @@
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean{
+    const request = context.switchToHttp().getRequest();
+    const isAuth = request.headers.auth === 'secret';
+    if(!isAuth) throw new UnauthorizedException("Not authorized");
+
+    return isAuth;
+  }
+}
